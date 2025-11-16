f26ef236ddd9310a9a54744843bddd647b1b1a99
alexandruSuciuRinf <125348012+alexandruSuciuRinf@users.noreply.github.com>
2025-11-16 19:36:43 +0200

    Add project files.

---
+++ Data/Suciu_Alexandru_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Suciu_Alexandru_Lab2.Models;
+
+namespace Suciu_Alexandru_Lab2.Data
+{
+    public class Suciu_Alexandru_Lab2Context : DbContext
+    {
+        public Suciu_Alexandru_Lab2Context (DbContextOptions<Suciu_Alexandru_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Suciu_Alexandru_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Suciu_Alexandru_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Suciu_Alexandru_Lab2.Data;
+using Suciu_Alexandru_Lab2.Models;
+
+namespace Suciu_Alexandru_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Suciu_Alexandru_Lab2.Data.Suciu_Alexandru_Lab2Context _context;
+
+        public IndexModel(Suciu_Alexandru_Lab2.Data.Suciu_Alexandru_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
